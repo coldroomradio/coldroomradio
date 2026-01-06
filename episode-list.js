5ff2127dc0f063db7957c8b7676ccdf5c7c4c4ed
coldroomradio <ptacoldroom@gmail.com>
2026-01-06 22:49:36 +0900

    Update episode list with new episode details

--- episode-list.js
+++ episode-list.js
@@ -5,8 +5,8 @@
 
 window.EPISODE_LIST = [
   { id:1, title:"<cold sleep>", date:"2025/10/10", desc:"2025/10/10 公開", src:"https://dn720703.ca.archive.org/0/items/cold-room-radio-episode-01-coldsleep/Cold%20Room%20Radio%20-%20Episode%2001%20-%20coldsleep.mp3", release_at: "2025-10-10T00:00:00+09:00" },
-  { id:2, title:"<are you ready!?>", date:"2026/01/01", desc:"2026/1/1", src:"https://dn710400.ca.archive.org/0/items/coldroomradio_ep02_areyouready/coldroomradio_ep02_areyouready.mp3", release_at: "2026-01-01T00:00:00+09:00" },
-  { id:3, title:"<echoes>", date:"2026/01/11", desc:"coming soon...", src:"", release_at: "2026-01-11T00:00:00+09:00" },
+  { id:2, title:"<are you ready!?>", date:"2026/01/01", desc:"2026/01/01", src:"https://dn710400.ca.archive.org/0/items/coldroomradio_ep02_areyouready/coldroomradio_ep02_areyouready.mp3", release_at: "2026-01-01T00:00:00+09:00" },
+  { id:3, title:"<echo>", date:"2026/01/11", desc:"coming soon...", src:"https://ia601707.us.archive.org/3/items/coldroomradio_ep03_echo/coldroomradio_ep03_echo.mp3", release_at: "2026-01-11T00:00:00+09:00" },
   { id:4, title:"<past memories>", date:"2026/01/18", desc:"coming soon...", src:"", release_at: "2026-01-18T00:00:00+09:00" },
   { id:5, title:"<K(p000003)>", date:"2026/02/01", desc:"coming soon...", src:"", release_at: "2026-02-01T00:00:00+09:00" },
   { id:6, title:"<N(p000002)>", date:"2026/02/08", desc:"coming soon...", src:"", release_at: "2026-02-08T00:00:00+09:00" },
