73c322e04e5ce128912075d1bba5c7221bfc4a80
coldroomradio <ptacoldroom@gmail.com>
2026-01-10 21:11:58 +0900

    Modify episode release dates and descriptions
    Updated release dates for episodes and adjusted descriptions.

--- episode-list.js
+++ episode-list.js
@@ -6,12 +6,12 @@
 window.EPISODE_LIST = [
   { id:1, title:"<cold sleep>", date:"2025/10/10", desc:"2025/10/10 公開", src:"https://dn720703.ca.archive.org/0/items/cold-room-radio-episode-01-coldsleep/Cold%20Room%20Radio%20-%20Episode%2001%20-%20coldsleep.mp3", release_at: "2025-10-10T00:00:00+09:00" },
   { id:2, title:"<are you ready!?>", date:"2026/01/01", desc:"2026/01/01", src:"https://dn710400.ca.archive.org/0/items/coldroomradio_ep02_areyouready/coldroomradio_ep02_areyouready.mp3", release_at: "2026-01-01T00:00:00+09:00" },
-  { id:3, title:"<echo>", date:"2026/01/11", desc:"coming soon...", src:"https://ia601707.us.archive.org/3/items/coldroomradio_ep03_echo/coldroomradio_ep03_echo.mp3", release_at: "2026-01-11T00:00:00+09:00" },
-  { id:4, title:"<past memories>", date:"2026/01/18", desc:"coming soon...", src:"", release_at: "2026-01-18T00:00:00+09:00" },
-  { id:5, title:"<K(p000003)>", date:"2026/02/01", desc:"coming soon...", src:"", release_at: "2026-02-01T00:00:00+09:00" },
-  { id:6, title:"<N(p000002)>", date:"2026/02/08", desc:"coming soon...", src:"", release_at: "2026-02-08T00:00:00+09:00" },
-  { id:7, title:"<A(p000001)>", date:"2026/02/15", desc:"coming soon...", src:"", release_at: "2026-02-15T00:00:00+09:00" },
-  { id:8, title:"<...>", date:"", desc:"coming soon...", src:"", release_at: null },
-  { id:9, title:"<...>", date:"", desc:"coming soon...", src:"", release_at: null },
-  { id:10, title:"<...>", date:"", desc:"coming soon...", src:"", release_at: null }
+  { id:3, title:"<echo>", date:"2026/01/11", desc:"coming soon...", src:"https://ia601707.us.archive.org/3/items/coldroomradio_ep03_echo/coldroomradio_ep03_echo.mp3", release_at: "2026-01-11T11:30:00+09:00" },
+  { id:4, title:"<past memories>", date:"2026/01/18", desc:"", src:"", release_at: "2026-01-18T11:30:00+09:00" },
+  { id:5, title:"<K(p000003)>", date:"2026/02/01", desc:"", src:"", release_at: "2026-02-01T11:30:00+09:00" },
+  { id:6, title:"<N(p000002)>", date:"2026/02/08", desc:"", src:"", release_at: "2026-02-08T11:30:00+09:00" },
+  { id:7, title:"<A(p000001)>", date:"2026/02/15", desc:"", src:"", release_at: "2026-02-15T11:30:00+09:00" },
+  { id:8, title:"<...>", date:"", desc:"", src:"", release_at: null },
+  { id:9, title:"<...>", date:"", desc:"", src:"", release_at: null },
+  { id:10, title:"<...>", date:"", desc:"", src:"", release_at: null }
 ];
