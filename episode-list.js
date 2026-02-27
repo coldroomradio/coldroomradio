b1e3df6c3a9eec28eb36a88dba8131e81ddae113
coldroomradio <ptacoldroom@gmail.com>
2026-02-27 21:31:26 +0900

    Update episode-list.js

--- episode-list.js
+++ episode-list.js
@@ -13,6 +13,6 @@ window.EPISODE_LIST = [
   { id:7, title:"<N(p000002)>", date:"2026/02/08公開", desc:"ココ好きシリーズ ②", src:"https://archive.org/download/coldroomradio_ep07_n/coldroomradio_ep07_n.mp3", release_at: "2026-02-08T18:59:00+09:00" },
   { id:8, title:"<A(p000001)>", date:"2026/02/15公開", desc:"ココ好きシリーズ ③", src:"https://archive.org/download/coldroomradio_ep08_a/coldroomradio_ep08_a.mp3", release_at: "2026-02-15T08:59:00+09:00" },
   { id:9, title:"<miracle love>", date:"2026/02/22公開", desc:"一番って決められる？-Song Edition-", src:"https://archive.org/download/coldroomradio_ep09_miraclelove/coldroomradio_ep09_miraclelove.mp3", release_at: "2026-02-22T18:59:00+09:00" },
-  { id:10, title:"<natural hello>", date:"2026/03/01公開", desc:"一番って決められる？-Dance Move Edition-", src:"", release_at: "2026-03-01T18:59:00+09:00"},
+  { id:10, title:"<natural hello>", date:"2026/03/01公開", desc:"一番って決められる？-Dance Move Edition-", src:"https://archive.org/download/coldroomradio_ep10_naturallove/coldroomradio_ep10_naturallove.mp3", release_at: "2026-03-01T18:59:00+09:00"},
   { id:11, title:"<...>", date:"2026/03/08公開", desc:"", src:"", release_at: "2026-03-08T18:59:00+09:00" }
 ];
