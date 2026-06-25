8dea88c31675d81e0dbd601d0b3e1ec5adfb9e88
coldroomradio <ptacoldroom@gmail.com>
2026-06-26 00:33:27 +0900

    Add episode 27: movie tour to episode list

--- episode-list.js
+++ episode-list.js
@@ -30,4 +30,6 @@ window.EPISODE_LIST = [
   { id:24, title:"<cold movie p3>", date:"2026/06/07公開", desc:"コールドスリーブ映画SP! Part 3", src:"https://archive.org/download/coldroomradio_ep24_csmoviep3/coldroomradio_ep24_csmoviep3.mp3", release_at: "2026-06-07T18:59:00+09:00" },
   { id:25, title:"<complete news>", date:"2026/06/14公開", desc:"完全版 DISC3!＋最新ニュース！", src:"https://archive.org/download/coldroomradio_ep25_completenews/coldroomradio_ep25_completenews.mp3", release_at: "2026-06-14T18:59:00+09:00" },
   { id:26, title:"<party party>", date:"2026/06/21公開", desc:"マイプレイリスト② W ver.", src:"https://archive.org/download/coldroomradio_ep26_partyparty/coldroomradio_ep26_partyparty.mp3", release_at: "2026-06-21T18:59:00+09:00" },
+  { id:27, title:"<movie tour>", date:"2026/06/28公開", desc:"舞台挨拶ツアー!", src:"https://archive.org/download/coldroomradio_ep27_movietour/coldroomradio_ep27_movietour.mp3", release_at: "2026-06-28T18:59:00+09:00" },
+
 ];
