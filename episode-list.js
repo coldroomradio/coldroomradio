be452b3492a068f559108962aa9288a284624e29
coldroomradio <ptacoldroom@gmail.com>
2026-07-25 23:00:01 +0900

    Add new episode 'dreams p2' to episode list

--- episode-list.js
+++ episode-list.js
@@ -34,5 +34,5 @@ window.EPISODE_LIST = [
   { id:28, title:"<summer news>", date:"2026/07/05公開", desc:"舞台挨拶ツアーのおかわり？雨だから聴く曲!", src:"https://archive.org/download/coldroomradio_ep28_summernews/coldroomradio_ep28_summernews.mp3", release_at: "2026-07-05T18:59:00+09:00" },
   { id:29, title:"<moon story>", date:"2026/07/12公開", desc:"昨年の今頃、何があったっけ？一緒に遡って行こう！", src:"https://archive.org/download/coldroomradio_ep29_moonstory/coldroomradio_ep29_moonstory.mp3", release_at: "2026-07-12T18:59:00+09:00" },
   { id:30, title:"<dreams p1>", date:"2026/07/19公開", desc:"みんな、「パフュー夢」見てる？①", src:"https://archive.org/download/coldroomradio_ep30_dreamsp1/coldroomradio_ep30_dreamsp1.mp3", release_at: "2026-07-19T18:59:00+09:00" },
-
+  { id:30, title:"<dreams p2>", date:"2026/07/26公開", desc:"みんな、「パフュー夢」見てる？②", src:"https://archive.org/download/coldroomradio_ep31_dreamsp2/coldroomradio_ep31_dreamsp2.mp3", release_at: "2026-07-26T18:59:00+09:00" },
 ];
